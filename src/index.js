2efb8950b24ef170f1cb0e1cd6da213effbc10bb
vgmda <vasilegancin@gmail.com>
2023-05-03 15:37:20 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { config } from 'dotenv';
+import { executeStudentCrudOperations } from './studentsCrud.js';
+
+config();
+await executeStudentCrudOperations();
+
+
+
---
+++ src/studentsCrud.js
@@ -0,0 +1,63 @@
+import {
+    MongoClient
+} from 'mongodb';
+
+
+export async function connectToCluster(uri) {
+    let mongoClient;
+
+    try {
+        mongoClient = new MongoClient(uri);
+        console.log('Connecting to MongoDB Atlas cluster...');
+        await mongoClient.connect();
+        console.log('Successfully connected to MongoDB Atlas!');
+
+        return mongoClient;
+    } catch (error) {
+        console.error('Connection to MongoDB Atlas failed!', error);
+        process.exit();
+    }
+}
+
+
+export async function executeStudentCrudOperations() {
+    const uri = process.env.DB_URI;
+    let mongoClient;
+
+    try {
+        mongoClient = await connectToCluster(uri);
+        const db = mongoClient.db('school');
+        const collection = db.collection('students');
+
+        //     console.log('CREATE Student');
+        //     await createStudentDocument(collection);
+        // } finally {
+        //     await mongoClient.close();
+        // }
+
+        console.log('DROP Student');
+        await dropStudentCollection(collection);
+    } finally {
+        await mongoClient.close();
+    }
+
+}
+
+export async function createStudentDocument(collection) {
+    const studentDocument = {
+        name: 'John Smith',
+        birthdate: new Date(2000, 11, 20),
+        address: {
+            street: 'Pike Lane',
+            city: 'Los Angeles',
+            state: 'CA'
+        },
+    };
+
+    await collection.insertOne(studentDocument);
+}
+
+export async function dropStudentCollection(collection) {
+
+    await collection.drop();
+}
